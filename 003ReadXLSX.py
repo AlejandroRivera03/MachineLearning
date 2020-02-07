14218c5cd6c2fee25a2f49ff188d508b2cce8bec
Alejandro <alejandro.rivera0309@gmail.com>
2020-02-06 23:59:46 -0600

    Reading xls files

---
+++ 003ReadXLSX.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+mainpath = 'F:/Cursos/Machine Learning/python-ml-course/datasets'
+filename = '/titanic/titanic3.xls'
+
+titanic3 = pd.read_excel(f'{mainpath}{filename}', 'titanic3')
+
+titanic3.to_csv(f'{mainpath}/titanic/mytitaniccustom.csv')
+titanic3.to_excel(f'{mainpath}/titanic/mytitaniccustom.xls')
+titanic3.to_json(f'{mainpath}/titanic/mytitaniccustom.json')
