acc2d10ba37c8bbd6b429d4483982d9c6e65f55a
Alejandro <alejandro.rivera0309@gmail.com>
2020-02-07 23:25:25 -0600

    Finishing the excercise about getting data from internet

--- 002ReadURL.py
+++ 002ReadURL.py
@@ -13,3 +13,34 @@ r = http.request('GET', url)
 response = r.data
 print(r.status)
 # print(response)
+
+str_data = response.decode('utf-8')
+
+lines = str_data.split('\n')
+
+col_names = lines[0].split(',')
+n_cols = len(col_names)
+
+counter = 0
+main_dict = {}
+for col in col_names:
+    main_dict[col] = []
+
+for line in lines:
+    if(counter > 0):
+        values = line.split(',')
+        for i in range(len(col_names)):
+            main_dict[col_names[i]].append(values[i])
+    counter += 1
+
+print(f'El data set tiene {counter} filas y {n_cols} columnas')
+
+medals_df = pd.DataFrame(main_dict)
+
+mainpath = 'F:/Cursos/Machine Learning/python-ml-course/datasets'
+filename = '/athletes/my_downloaded_medals.'
+fullpath = mainpath+filename
+
+medals_df.to_csv(fullpath+'csv')
+medals_df.to_json(fullpath+'json')
+medals_df.to_excel(fullpath+'xlsx')
