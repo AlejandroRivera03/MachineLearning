cd62f404f3090bcf99e0974d6a93d207a27b2cd5
Alejandro <alejandro.rivera0309@gmail.com>
2020-02-21 23:17:07 -0600

    Creating subsets getting rows by conditionals

--- 010DataWrangling.py
+++ 010DataWrangling.py
@@ -22,6 +22,31 @@ print(subset.head())
 # Indicating not desired columns
 not_desired_columns = ['Account Length', 'Phone', 'Eve Charge', 'Night Calls', 'CustServ Calls', 'VMail Message', 'VMail Plan', 'Int\'l Plan']
 all_columns_list = data.columns.values.tolist()
-sublist = [x for x in all_columns_list if x not in not_desired_columns]
+# sublist = [x for x in all_columns_list if x not in not_desired_columns]
+sublist = list(set(all_columns_list)-set(not_desired_columns))
 subset = data[sublist]
 print(subset.head())
+
+# print specific row by indexes
+print(data[100:115])
+
+# sub set by conditional
+# rows where 'Day Mins' more than 300
+data1 = data[data['Day Mins']>300]
+print(f'rows where \'Day Mins\' more than 300 =>  {data1.shape}')
+
+# rows where 'State' equal than NY (New York)
+data2 = data[data['State']=='NY']
+print(f'rows where \'State\' equal than NY (New York) => {data2.shape}')
+
+# rows where 'Day Mins' more than 300 and 'State' equal than NY
+data3 = data[(data['Day Mins']>300) & (data['State']=='NY')]
+print(f'rows where \'Day Mins\' more than 300 and \'State\' equal than NY => {data3.shape}')
+
+# rows where 'Day Mins' more than 300 or 'State' equal than NY
+data4 = data[(data['Day Mins']>300) | (data['State']=='NY')]
+print(f'rows where \'Day Mins\' more than 300 or \'State\' equal than NY => {data4.shape}')
+
+# rows where 'Night Calls' are more that 'Day Calls'
+data5 = data[(data['Day Calls']<data['Night Calls'])]
+print(f'rows where \'Night Calls\' are more that \'Day Calls\' => {data5.shape}')
