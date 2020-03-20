0d440237575473dd17fdb6d5b34d9528f7f8fe31
Alejandro <alejandro.rivera0309@gmail.com>
2020-03-19 23:45:11 -0600

    Calculating the linear regression

--- 025LinearRegressionTest.py
+++ 025LinearRegressionTest.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # ye = 5 + 1.9*x + e
 # e will be normally distributed N(0, 0.8)      Media => 0      desviacion tipica => 0.8
 
-np.random.seed(19940903)
+# np.random.seed(19940903)
 
 x = 1.5 + 2.5 * np.random.randn(100) # 100 normally distributed values for 'x'
 
@@ -57,3 +57,44 @@ print( f'SSD => {sum(data["SSD"])}' )
 print( f'SST => {sum(data["SST"])}' )
 print( f'SSR+SSD = SST  => {sum(data["SSR"]) + sum(data["SSD"])}' )
 print( f'R2 = SSR/SST => {sum(data["SSR"]) / sum(data["SST"])}' )
+
+
+# CALCULANDO LA RECTA
+# y = a + b*x
+# b = sum( (xi - x_m)*(yi - y_m) ) / sum( (xi - x_m)^2 )
+# a = y_m - b * x_m
+
+print('\nCalculando la recta\n')
+
+x_mean = np.mean(data['x'])
+y_mean = np.mean(data['y_actual'])
+print(f'x_mean => {x_mean},  y_mean => {y_mean}')
+
+data['beta_n'] = (data['x']-x_mean)*(data['y_actual']-y_mean)
+data['beta_d'] = (data['x']-x_mean)**2
+
+beta = sum(data['beta_n']) / sum(data['beta_d'])
+alpha = y_mean - beta * x_mean
+print(f'alpha => {alpha},  beta => {beta}')
+
+data['y_model'] = alpha + beta * data['x']
+
+# print(data.head())
+
+SSR = sum( (data['y_model']-y_mean)**2 )
+SSD = sum( (data['y_model']-data['y_actual'])**2 )
+SST = sum( (data['y_actual']-y_mean)**2 )
+
+print(f'SSR => {SSR}')
+print(f'SSD => {SSD}')
+print(f'SST => {SST}')
+
+print(f'SSR/SST => {SSR/SST}' )
+
+y_mean = [np.mean(y_actual) for i in range(1, len(x_list) + 1)]
+plt.plot(data['x'], data['y_prediccion'])
+plt.plot(data['x'], data['y_actual'], 'ro')
+plt.plot(data['x'], y_mean, 'g')
+plt.plot(data['x'], data['y_model'])
+plt.title('Valor Actual vs Predicción')
+plt.show()
