d546b586841de3a631df0beaef08847a4b095bdb
Alejandro <alejandro.rivera0309@gmail.com>
2020-03-28 00:38:42 -0600

    Removing more 'outliers' to notice more the model improvement, only for test, it must not remove more that 5%

--- 032Outliers.py
+++ 032Outliers.py
@@ -4,12 +4,14 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv('./datasets/auto/auto-mpg.csv')
+data['displacement'] = data['displacement'].fillna(data['displacement'].mean())
+data['mpg'] = data['mpg'].fillna(data['mpg'].mean())
 
 print(data.shape)
 
-X = data['displacement'].fillna(data['displacement'].mean())
+X = data['displacement']
 X = X[:,np.newaxis]
-Y = data['mpg'].fillna(data['mpg'].mean())
+Y = data['mpg']
 
 lm = LinearRegression()
 lm.fit(X, Y)
@@ -20,18 +22,18 @@ plt.plot(X, Y, 'ro')
 plt.plot(X, lm.predict(X), color='blue')
 # plt.show()
 
-
-# print(data[(data['displacement']>250)&(data['mpg']>35)])
-# print(data[(data['displacement']>300)&(data['mpg']>20)])
-# print(data[(data['displacement']<150)&(data['mpg']>40)])
+indexesToRemove = []
+indexesToRemove[len(indexesToRemove):] = data[(data['displacement']>250)&(data['mpg']>35)].index.tolist()
+indexesToRemove[len(indexesToRemove):] = data[(data['displacement']>300)&(data['mpg']>20)].index.tolist()
+indexesToRemove[len(indexesToRemove):] = data[(data['displacement']<150)&(data['mpg']>35)].index.tolist()
+print(f'{len(indexesToRemove)} rows removed to see improvement (10% aprox), but it must remove less percent')
 
 # dropping some outliers by index
-# indexes were taken from the result of the before prints
-data_clean = data.drop([395, 258, 305, 372, 251, 316, 329, 331, 332, 333, 336, 337, 402]) 
+data_clean = data.drop(indexesToRemove) 
 
-X = data_clean['displacement'].fillna(data_clean['displacement'].mean())
+X = data_clean['displacement']
 X = X[:,np.newaxis]
-Y = data_clean['mpg'].fillna(data_clean['mpg'].mean())
+Y = data_clean['mpg']
 
 lm = LinearRegression()
 lm.fit(X, Y)
@@ -39,5 +41,6 @@ lm.fit(X, Y)
 # without outliers
 print(f'without outliers => {lm.score(X,Y)}') 
 plt.plot(X, Y, 'go')
+plt.title('Red ones (outliers) were remove to improve model')
 # plt.plot(X, lm.predict(X), color='blue')
 plt.show()
