48db0589f998fb29721162f5e3e4f5a94f9b302a
Alejandro <alejandro.rivera0309@gmail.com>
2020-04-01 21:54:29 -0600

    odds ratio

--- 033LogisticRegression.py
+++ 033LogisticRegression.py
@@ -27,3 +27,27 @@ print(f'\nP(Purchase | Male) => {121/246}')
 print(f'\nP(No purchase | Male) => {125/246}')
 print(f'\nP(Purchase | Female) => {159/265}')
 print(f'\nP(No purchase | Female) => {106/265}')
+
+
+# RATIO DE PROBABILIDADES (Cociente de probabilidades)
+
+# Cociente = (0, infinito)
+# Cociente < 1  =>  Mas probable el fracaso que el exito
+# Cociente = 1  =>  Equiprobable (misma probabilidad de que pase a que no pase)
+# Cociente > 1  =>  Mas probable el exito que el fracaso (puede tender a infinito)
+
+# pm = probabilidad de compra sabiendo que es hombre
+# pf = probabilidad de compra sabiendo que es mujer
+
+# cocientem   =   pm / (1 - pm)   =   pm / ~pm 
+# cocientef   =   pf / (1 - pf)   =   pf / ~pf
+
+pm = 121 / 246
+pf = 159 / 265
+cocientem = pm/(1-pm)
+cocientef = pf/(1-pf)
+print(f'male probability ratio => {cocientem}')
+print(f'female probability ratio => {cocientef}')
+
+print(121/125)
+print(159/106)
