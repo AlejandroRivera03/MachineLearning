55a5e745160cc57aa0dd44c8747426bc40f5fd28
Alejandro <alejandro.rivera0309@gmail.com>
2020-04-13 21:02:55 -0500

    Making a dendogram by joining clusters

--- 036Distances.py
+++ 036Distances.py
@@ -2,6 +2,7 @@ from scipy.spatial import distance_matrix
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.cluster.hierarchy import dendrogram, linkage
 
 def distance_matrix_to_dataframe(dd, col_name):
     return pd.DataFrame(dd, index=col_name, columns=col_name)
@@ -26,4 +27,16 @@ ax.scatter(xs=data['star_wars'], ys=data['lord_of_the_rings'], zs=data['harry_po
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
+plt.show()
+
+# CLUSTER JERARQUICO
+
+Z = linkage(data[movies], 'ward') # options => 'ward' | 'average' | 'complete' | 'single' | 'weighted' | 'centroid' | 'median'
+print(f'Cluster1 | Cluster2 | Distance | individual clusters joined\n{Z}')
+
+plt.figure(figsize=(25, 10))
+plt.title('Diagrama jerarquico para el clustering')
+plt.xlabel('ID de los usuarios de netflix')
+plt.ylabel('Distancia')
+dendrogram(Z, leaf_rotation=90.0, leaf_font_size=10)
 plt.show()
