976055220291e48b36b946b317fed27681ab8647
Alejandro <alejandro.rivera0309@gmail.com>
2020-05-13 21:32:23 -0500

    K Nearest Neighboors

--- 052KNearestNeighbors.py
+++ 052KNearestNeighbors.py
@@ -8,8 +8,30 @@ print(f'Dataset =>\n{df.head()}')
 
 df.columns = ['name', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'class'] # Setting names to columns
 
-df = df.drop(['name'], 1) # Removing this unnecessary column
+df.replace('?', -99999, inplace=True) # there are '?' in the dataset
 Y = df['class']
-X = df[['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9']]
+X = df[['name', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9']]
 print(f'Predictor Variables =>\n{X.head()}')
 print(f'Target =>\n{Y.head()}')
+
+# Clasificacion de los K Vecinos
+#   Sin limpieza
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, Y_train)
+
+accuracy = clf.score(X_test, Y_test)
+print(f'Accuracy without data cleaning => {accuracy}')
+
+
+#   Con limpieza
+df = df.drop(['name'], 1) # Removing this unnecessary column
+X = df[['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9']] # The difference is here, skip 'name' column
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, Y_train)
+
+accuracy = clf.score(X_test, Y_test)
+print(f'Accuracy with data cleaning => {accuracy}')
