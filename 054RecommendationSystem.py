82dfe378db4af7ed07c8d89663515c58943dc67e
Alejandro <alejandro.rivera0309@gmail.com>
2020-05-15 23:56:20 -0500

    Creating a sparsity matrix to contrast users and users' movies rated

--- 054RecommendationSystem.py
+++ 054RecommendationSystem.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -20,3 +21,21 @@ plt.show()
 # Most rated movies
 plt.hist(df.groupby(['ItemID'])['ItemID'].count())
 plt.show()
+
+n_users = df.UserID.unique().shape[0]
+n_items = df.ItemID.unique().shape[0]
+
+print(f'Numero total de usuarios => {n_users}\nNumero total de peliculas => {n_items}')
+
+# Creating a matrix to contrast users and users' movies rated
+ratings = np.zeros((n_users, n_items))
+
+for row in df.itertuples():
+    ratings[row[1]-1, row[2]-1] = row[3]
+
+# print(ratings)
+
+sparsity = float(len(ratings.nonzero()[0]))
+sparsity /= (ratings.shape[0]*ratings.shape[1])
+sparsity *= 100
+print('Coeficiente de sparseidad: {:4.2f}%'.format(sparsity))
