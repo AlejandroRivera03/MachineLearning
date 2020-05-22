b669e01d8ded1a70d48957fe7e8164ec2a716524
Alejandro <alejandro.rivera0309@gmail.com>
2020-05-21 23:28:35 -0500

    Getting the own values and vectors with 3 different methods

--- 055ACP.py
+++ 055ACP.py
@@ -6,6 +6,7 @@
 # 4 - Contruir la matriz de proyeccion 'W' a partir de los 'p' vectores propios
 # 5 - Tranformar el dataset original 'X' a traves de 'W' para asi obtener datos en el subespacio dimensional de dimension 'p', que sera 'Y'
 
+import numpy as np
 import pandas as pd
 import plotly as py
 import plotly.tools as tls
@@ -63,3 +64,34 @@ layout = ir.Layout(barmode='overlay',
 
 fig = ir.Figure(data=data, layout=layout)
 fig.show()
+
+# Point 2 (Obtener los vectores y valores propios 'matrix de covarianza')
+# Vector de las medias
+mean_vect = np.mean(X_std, axis=0)
+print(f'Vector de las medias \n{mean_vect}')
+
+# Matriz de covarianzas
+cov_matrix = (X_std - mean_vect).T.dot((X_std - mean_vect))/(X_std.shape[0]-1)
+print(f'La matriz de covarianzas es \n{cov_matrix}') # 1's en diagonal (aprox)
+
+np.cov(X_std.T)
+
+# Vectores y valores propios (con libreria)
+eig_vals, eig_vectors = np.linalg.eig(cov_matrix)
+print(f'Valores propios \n{eig_vals}')
+print(f'Vectores propios \n{eig_vectors}')
+
+# Point 2 (Obtener los vectores y valores propios 'matriz de correlaciones')
+# Matriz de correlaciones
+corr_matrix = np.corrcoef(X_std.T)
+print(f'La matriz de correlaciones \n{corr_matrix}')
+
+eig_vals_corr, eig_vectors_corr = np.linalg.eig(corr_matrix)
+print(f'Valores propios \n{eig_vals_corr}')
+print(f'Vectores propios \n{eig_vectors_corr}')
+
+# Point 2 (Obtener los vectores y valores propios 'singular value decomposition')
+u,s,v = np.linalg.svd(X_std.T)
+print(f'u =>\n{u}')
+print(f's =>\n{s}')
+print(f'v =>\n{v}')
